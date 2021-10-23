d3f80728e5465f430e1c9b2a989f707406d3cd5e
bibolil <80677927+bibolil@users.noreply.github.com>
2021-10-23 20:43:51 +0100

    Update Main.java

--- src/tp2/magicFile/Main.java
+++ src/tp2/magicFile/Main.java
@@ -1,6 +1,7 @@
 package tp2.magicFile;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
@@ -19,16 +20,23 @@ public class Main {
     // printFileContent that prints it s content otherwise print 'file not found :/'
     // todo : implement the methods and force exception handling in the printFile method
 
-    private static String getFileContent(String fileName) {
-        File file = new File("src/tp2/magicFile/" + fileName);
+    private static String getFileContent(String fileName) throws FileNotFoundException {
+        File file = new File("src/" + fileName);
         //the lines below help you to get the content
-//        Scanner reader = new Scanner(file);
-//        String content = reader.nextLine();
-        return null;
+        Scanner reader = new Scanner(file);
+        String content = reader.nextLine();
+        return content;
     }
 
     private static void printFileContent(String fileName) {
-        String data = getFileContent(fileName);
-        System.out.println(data);
+        String data = null;
+        try {
+            data = getFileContent(fileName);
+            System.out.println(data);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+
+
     }
 }
